518ff1ded1d4138955b19691521d709e8dab5a0b
red-gezi <506065448@qq.com>
2021-03-28 22:50:29 +0800

    更新 重奏 停止 字母谱识别

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,13 +33,17 @@
             this.btn_play = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.text_music = new System.Windows.Forms.TextBox();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.cb_mode = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.delayTime = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn_play
             // 
-            this.btn_play.Location = new System.Drawing.Point(313, 329);
+            this.btn_play.Location = new System.Drawing.Point(110, 338);
             this.btn_play.Name = "btn_play";
-            this.btn_play.Size = new System.Drawing.Size(75, 23);
+            this.btn_play.Size = new System.Drawing.Size(163, 63);
             this.btn_play.TabIndex = 0;
             this.btn_play.Text = "演奏";
             this.btn_play.UseVisualStyleBackColor = true;
@@ -53,18 +57,60 @@
             // 
             // text_music
             // 
-            this.text_music.Location = new System.Drawing.Point(21, 12);
+            this.text_music.Location = new System.Drawing.Point(25, 49);
             this.text_music.Multiline = true;
             this.text_music.Name = "text_music";
             this.text_music.Size = new System.Drawing.Size(698, 274);
             this.text_music.TabIndex = 1;
             this.text_music.Text = resources.GetString("text_music.Text");
             // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(463, 338);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(163, 63);
+            this.btn_Stop.TabIndex = 2;
+            this.btn_Stop.Text = "停止";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // cb_mode
+            // 
+            this.cb_mode.FormattingEnabled = true;
+            this.cb_mode.Items.AddRange(new object[] {
+            "数字",
+            "字母"});
+            this.cb_mode.Location = new System.Drawing.Point(48, 13);
+            this.cb_mode.Name = "cb_mode";
+            this.cb_mode.Size = new System.Drawing.Size(121, 23);
+            this.cb_mode.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "间隔频率：";
+            // 
+            // delayTime
+            // 
+            this.delayTime.Location = new System.Drawing.Point(319, 13);
+            this.delayTime.Name = "delayTime";
+            this.delayTime.Size = new System.Drawing.Size(100, 25);
+            this.delayTime.TabIndex = 5;
+            this.delayTime.Text = "300";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(752, 450);
+            this.Controls.Add(this.delayTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb_mode);
+            this.Controls.Add(this.btn_Stop);
             this.Controls.Add(this.text_music);
             this.Controls.Add(this.btn_play);
             this.Name = "Form1";
@@ -79,6 +125,10 @@
         private System.Windows.Forms.Button btn_play;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox text_music;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.ComboBox cb_mode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox delayTime;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,8 @@ namespace 原神自动弹奏器
 {
     public partial class Form1 : Form
     {
+        bool isStop;
+        Random rand = new Random();
 
         public Form1()
         {
@@ -21,23 +23,45 @@ namespace 原神自动弹奏器
         string key = "";
         private void btn_play_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("开始播放");
+            isStop = false;
+            Console.WriteLine("\n演唱~开始♩");
             List<string> music_score = new List<string>();
-            foreach (Match item in Regex.Matches(text_music.Text, @"\d\'|\d| "))
+            MatchCollection matchCollection;
+            if (cb_mode.SelectedIndex == 0)
+            {
+                matchCollection = Regex.Matches(text_music.Text, @"\(\d*\)|\d#|\d'|\d| ");
+            }
+            else
+            {
+                matchCollection = Regex.Matches(text_music.Text, @"\(\w*\)|\w| ");
+            }
+            foreach (Match item in matchCollection)
             {
                 music_score.Add(item.Value);
-                //Console.WriteLine(item.Value);
             }
             Task.Run(async () =>
-            {
-                await Task.Delay(3000);
-                for (int i = 0; i < music_score.Count; i++)
-                {
-                    key = GetKeyMap(music_score[i]);
-                    await Task.Delay(300);
-                    Console.Write(music_score[i]);
-                }
-            });
+              {
+                  await Task.Delay(3000);
+                  for (int i = 0; i < music_score.Count; i++)
+                  {
+                      music_score[i] = music_score[i].Replace("(", "").Replace(")", "");
+                      for (int j = 0; j < music_score[i].Count(); j++)
+                      {
+                          key = GetKeyMap(music_score[i][j].ToString());
+                          Console.Write(music_score[i][j].ToString());
+                          await Task.Delay(rand.Next(10));
+                      }
+                      await Task.Delay(int.Parse(delayTime.Text) + rand.Next(20));
+                      if (isStop)
+                      {
+                          break;
+                      }
+                  }
+              });
+        }
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            isStop = true;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -48,18 +72,18 @@ namespace 原神自动弹奏器
                 key = "";
             }
         }
-        public string GetKeyMap(string number)
+        public string GetKeyMap(string keycode)
         {
             string key = "";
-            switch (number)
+            switch (keycode)
             {
-                case "1": key="a";break;
-                case "2": key="s";break;
-                case "3": key="d";break;
-                case "4": key="f";break;
-                case "5": key="g";break;
-                case "6": key="h";break;
-                case "7": key="j";break;
+                case "1": key = "a"; break;
+                case "2": key = "s"; break;
+                case "3": key = "d"; break;
+                case "4": key = "f"; break;
+                case "5": key = "g"; break;
+                case "6": key = "h"; break;
+                case "7": key = "j"; break;
 
                 case "1'": key = "q"; break;
                 case "2'": key = "w"; break;
@@ -68,9 +92,19 @@ namespace 原神自动弹奏器
                 case "5'": key = "t"; break;
                 case "6'": key = "y"; break;
                 case "7'": key = "u"; break;
-                default:break;
+
+                case "1#": key = "z"; break;
+                case "2#": key = "x"; break;
+                case "3#": key = "c"; break;
+                case "4#": key = "v"; break;
+                case "5#": key = "b"; break;
+                case "6#": key = "n"; break;
+                case "7#": key = "m"; break;
+                case " ": key = " "; break;
+                default: key = keycode; break;
             }
             return key;
         }
+
     }
 }
