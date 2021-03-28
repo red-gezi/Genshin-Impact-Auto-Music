63c37235186e2d7dcc46b804cf8fcfa46c289a2f
red-gezi <506065448@qq.com>
2021-03-28 10:31:35 +0800

    初始化

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace 原神自动弹奏器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_play = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.text_music = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_play
+            // 
+            this.btn_play.Location = new System.Drawing.Point(313, 329);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(75, 23);
+            this.btn_play.TabIndex = 0;
+            this.btn_play.Text = "演奏";
+            this.btn_play.UseVisualStyleBackColor = true;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // text_music
+            // 
+            this.text_music.Location = new System.Drawing.Point(21, 12);
+            this.text_music.Multiline = true;
+            this.text_music.Name = "text_music";
+            this.text_music.Size = new System.Drawing.Size(698, 274);
+            this.text_music.TabIndex = 1;
+            this.text_music.Text = resources.GetString("text_music.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.text_music);
+            this.Controls.Add(this.btn_play);
+            this.Name = "Form1";
+            this.Text = "原神自动演奏机 -- made by 格子";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox text_music;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 原神自动弹奏器
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string key = "";
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("开始播放");
+            List<string> music_score = new List<string>();
+            foreach (Match item in Regex.Matches(text_music.Text, @"\d\'|\d| "))
+            {
+                music_score.Add(item.Value);
+                //Console.WriteLine(item.Value);
+            }
+            Task.Run(async () =>
+            {
+                await Task.Delay(3000);
+                for (int i = 0; i < music_score.Count; i++)
+                {
+                    key = GetKeyMap(music_score[i]);
+                    await Task.Delay(300);
+                    Console.Write(music_score[i]);
+                }
+            });
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (key != "")
+            {
+                SendKeys.Send(key);
+                key = "";
+            }
+        }
+        public string GetKeyMap(string number)
+        {
+            string key = "";
+            switch (number)
+            {
+                case "1": key="a";break;
+                case "2": key="s";break;
+                case "3": key="d";break;
+                case "4": key="f";break;
+                case "5": key="g";break;
+                case "6": key="h";break;
+                case "7": key="j";break;
+
+                case "1'": key = "q"; break;
+                case "2'": key = "w"; break;
+                case "3'": key = "e"; break;
+                case "4'": key = "r"; break;
+                case "5'": key = "t"; break;
+                case "6'": key = "y"; break;
+                case "7'": key = "u"; break;
+                default:break;
+            }
+            return key;
+        }
+    }
+}
